b7fba9aba918578744e5a7bbd878bd393b8cb143
Ansyar <ansyar@me.com>
2025-03-06 19:04:55 +0700

    Adding ErrorResponse

--- Sources/ModernSwiftNetworking/NetworkService.swift
+++ Sources/ModernSwiftNetworking/NetworkService.swift
@@ -8,44 +8,61 @@
 import Foundation
 import Combine
 
+public enum ErrorResponse: Error {
+    case invalidResponse(statusCode: Int)
+    case emptyData(statusCode: Int)
+    case decodingError(underlying: Error, statusCode: Int)
+}
+
 public struct NetworkService: @unchecked Sendable {
     public static let shared = NetworkService()
     
-    public func request<T: Decodable>(_ target: TargetType, decodingType: T.Type) -> AnyPublisher<T, Error> {
+    public func request<T: Decodable>(_ target: TargetType, decodingType: T.Type) -> AnyPublisher<T, ErrorResponse> {
         let request = buildRequest(from: target)
         
         return URLSession.shared.dataTaskPublisher(for: request)
-            .tryMap { data, response -> Data in
-                guard let httpResponse = response as? HTTPURLResponse, 200..<300 ~= httpResponse.statusCode else {
-                    throw URLError(.badServerResponse)
-                }
-                return data
+            .mapError { error in
+                ErrorResponse.invalidResponse(statusCode: -1)
             }
-            .flatMap { data -> AnyPublisher<T, Error> in
+            .tryMap { data, response -> (Data, HTTPURLResponse) in
+                guard let httpResponse = response as? HTTPURLResponse else {
+                    throw ErrorResponse.invalidResponse(statusCode: -1)
+                }
+                guard 200..<300 ~= httpResponse.statusCode else {
+                    throw ErrorResponse.invalidResponse(statusCode: httpResponse.statusCode)
+                }
+                return (data, httpResponse)
+            }
+            .mapError { error in
+                error as? ErrorResponse ?? ErrorResponse.invalidResponse(statusCode: -1)
+            }
+            .flatMap { data, httpResponse -> AnyPublisher<T, ErrorResponse> in
                 // Handle empty response with EmptyResponse
                 if data.isEmpty {
                     // Attempt to return an empty instance of T if possible
                     if let emptyInstance = try? JSONDecoder().decode(T.self, from: "{}".data(using: .utf8)!) {
                         return Just(emptyInstance)
-                            .setFailureType(to: Error.self)
+                            .setFailureType(to: ErrorResponse.self)
+                            .eraseToAnyPublisher()
+                    } else {
+                        return Fail(error: ErrorResponse.emptyData(statusCode: httpResponse.statusCode))
                             .eraseToAnyPublisher()
                     }
-                    
-                    // Otherwise, throw an error or handle it gracefully
-                    return Fail(error: URLError(.cannotDecodeRawData))
-                        .eraseToAnyPublisher()
                 }
                 
                 // Check if the decodingType is Data itself
                 if decodingType == Data.self, let castedData = data as? T {
                     return Just(castedData)
-                        .setFailureType(to: Error.self)
+                        .setFailureType(to: ErrorResponse.self)
                         .eraseToAnyPublisher()
                 }
 
                 // Otherwise, decode normally
                 return Just(data)
                     .decode(type: T.self, decoder: JSONDecoder())
+                    .mapError { error in
+                        ErrorResponse.decodingError(underlying: error, statusCode: httpResponse.statusCode)
+                    }
                     .eraseToAnyPublisher()
             }
             .receive(on: DispatchQueue.main)
