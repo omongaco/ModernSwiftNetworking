b608f3e51e4d9615442b3c2d65d4edae66b60147
Omong Aco <omongaco@gmail.com>
2025-05-16 15:09:20 +0700

    Update Package.swift for iOS deployent target

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "ModernSwiftNetworking",
     platforms: [
-        .iOS(.v13), .macOS(.v10_15), .tvOS(.v13), .watchOS(.v6)
+        .iOS(.v15), .macOS(.v10_15), .tvOS(.v13), .watchOS(.v6)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
